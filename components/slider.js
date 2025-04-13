cd427a4093b04b462c739e1cf3de85b2fd4d26ec
nematnejad <nematnejad>
2025-04-13 16:51:42 +0330

    modify slider

--- components/slider.js
+++ components/slider.js
@@ -1,4 +1,4 @@
-import { Avatar, Box, Button, Flex, Text } from "@chakra-ui/react";
+import { Avatar, Box, Button, Flex, Text, VStack } from "@chakra-ui/react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
@@ -16,28 +16,32 @@ const sliderSettings = {
 
 
 
-const SliderCom = ({ items, height, borderRadius, width = 'auto' }) => {
+const SliderCom = ({ items, height, borderRadius, width = 'auto', title }) => {
   return (
-    <Box w="100%" alignItems={'center'} justifyContent={'center'} maxW="container.xl" mx="auto" p={'60px'} border={'1px'} borderColor={'gray.200'} borderRadius={'16px'} height={height} my={'20px'}>
-      <Slider {...sliderSettings}>
-        {items.map((item, index) => (
-          <Flex
-            textAlign={'center'}
-            w="100%"
-            h="100%"
-            key={index}
-            p={'10px'}
-            direction="column"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Avatar w="128px" h="128px" borderRadius={borderRadius} />
-            {item.title && <Text fontWeight="bold" my={'20px'}>{item.title}</Text>}
-            {item.button && <Button bgColor={'#29CCCC'}>{item.button}</Button>}
-          </Flex>
-        ))}
-      </Slider>
-    </Box>
+    <VStack w={'100%'} border={'1px'} borderColor={'gray.200'} borderRadius={'16px'} p={'40px'} alignItems={'start'} height={height} my={'20px'}>
+      <Text fontWeight={'bold'}>{title}</Text>
+      <Box w="100%" alignItems={'center'} justifyContent={'center'} mx="auto" >
+        <Slider {...sliderSettings}>
+          {items.map((item, index) => (
+            <Flex
+              textAlign={'center'}
+              w="100%"
+              h="100%"
+              key={index}
+              p={'10px'}
+              direction="column"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Avatar w="128px" h="128px" borderRadius={borderRadius} />
+              {item.title && <Text fontWeight="bold" my={'20px'}>{item.title}</Text>}
+              {item.button && <Button bgColor={'#29CCCC'}>{item.button}</Button>}
+            </Flex>
+          ))}
+        </Slider>
+      </Box>
+    </VStack>
+
   )
 }
 
